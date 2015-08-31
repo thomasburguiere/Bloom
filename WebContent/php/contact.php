4d91d1db5566bf3247a44bb687debf889656ac75
= <=>
2015-08-31 17:33:49 +0200

    reinitialisation

---
+++ WebContent/php/contact.php
@@ -0,0 +1,21 @@
+<?php        
+    echo "magna";
+           /* echo $_POST['nom'];
+                if(isset($_POST) && isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['message'])){
+                    if(!empty($_POST['nom']) && !empty($_POST['email']) && !empty($_POST['message'])){
+                        $destinataire = "melanie.hachet@gmail.com";
+                        $sujet = "Demande de contact";
+                        $message = "Nom : ".$_POST['nom']."\r\n";
+                        $message = "Adresse email : ".$_POST['email']."\r\n";
+                        $message = "Message : ".$_POST['message']."\r\n";
+                        $entete = 'From: '.$_POST['email']."\r\n".
+                            'Reply-To: '.$_POST['email']."\r\n".
+                        'X-Mailer: PHP/'.phpversion();
+                        if (mail($destinataire,$sujet,$message,$entete)){
+                            echo 'Message envoyé';
+                        } else {
+                            echo "Une erreur est survenue lors de l'envoi du formulaire par email";
+                        }
+                    }
+                }*/
+?>
