1988ef7eac7a83fd53d4af03759fae8fc3ce830c
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-13 02:56:56 +0100

    [app_architecture] setting up jersey & jackson for webservice

---
+++ src/main/java/fr/bird/bloom/servlets/WebServiceController.java
@@ -0,0 +1,33 @@
+package fr.bird.bloom.servlets;
+
+
+import fr.bird.bloom.dto.ServiceInput;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/bloomWorkflow")
+public class WebServiceController {
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String landing(){
+
+        return "it works over GET";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    public String process(ServiceInput input) {
+
+
+        return "it works over POST";
+    }
+
+}
